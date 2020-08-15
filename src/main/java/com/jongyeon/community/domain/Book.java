878ef85487baa32c3e76954112aa62277b10f341
whddus9509 <whddus9509@naver.com>
2020-08-15 20:33:20 +0900

    @DataJpaTest

--- src/main/java/com/jongyeon/community/domain/Book.java
+++ src/main/java/com/jongyeon/community/domain/Book.java
@@ -2,16 +2,26 @@ package com.jongyeon.community.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Controller;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
-@Data
+@Entity
+@Table
+@Getter
 public class Book {
+    @Id
+    @GeneratedValue
     private Integer idx;
+    @Column
     private String title;
+    @Column
     private LocalDateTime publishedAt;
 
     @Builder
